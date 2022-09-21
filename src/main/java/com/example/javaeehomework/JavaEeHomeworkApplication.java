76ac1d38aebbc6037aa26c5879d3c0b170e5483a
Lunr_Lee <Lunr_Lee@outlook.com>
2022-09-21 16:54:06 +0800

    Initial commit

---
+++ src/main/java/com/example/javaeehomework/JavaEeHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaeehomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEeHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaEeHomeworkApplication.class, args);
+    }
+
+}
